310eb8fa8fdb9054d788825ddda5ea7ac10dc2ba
Tanya Jain <tanya.jain@consulting.active.ai>
2020-06-30 08:59:13 +0530

    Initial React App setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Widget from './components/widget';
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Widget/>
+    </React.StrictMode>,
+    document.getElementById('container')
+);
---
+++ widget/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        },
+        output: {
+            filename: 'widget.bundle.js',
+            path: path.resolve(__dirname, '../public'),
+            },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ],
+   
+  },
+  plugins: [htmlWebpackPlugin]
+};
